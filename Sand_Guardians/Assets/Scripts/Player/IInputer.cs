74b8c9ec16b143c146ea00ce422aabdabef79427
GTA20MiwaYuga <ym2054@mailg.denpa.ac.jp>
2021-06-18 10:15:04 +0900

    Player更新

---
+++ Sand_Guardians/Assets/Scripts/Player/IInputer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    interface IInputer
+    {
+        float Move();
+
+        bool Attack();
+
+        bool SpecialAttack();
+    }
+}

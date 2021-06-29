3a8db874f66bee5072a805432b38feb9f31cea6b
DESKTOP-E8EHEE1\user <breakretstoda@ezweb.ne.jp>
2021-06-29 10:53:20 +0900

    Enemy処理更新

---
+++ Sand_Guardians/Assets/Scripts/TitleScene/PressAnyKey.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressAnyKey : MonoBehaviour
+{
+    // テキストの透明度を変化させる処理
+
+    // クラス変数
+    Color color = new Vector4(0, 0, 0, 0.8f); // 半透明状態変数
+    Text text;
+
+    // 透明度切り替え用フラグ
+    bool up;
+
+
+    void Start()
+    {
+        // コンポーネントを取得
+        text = this.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        // 透明度変更処理
+        // 透明度が変わったり元に戻ったりする
+        if (text.color.a >= 0.8f) { up = false; }
+        if (text.color.a <= 0.2f) { up = true; }
+        if (up == true) { text.color += color * Time.deltaTime; }
+        if (up == false) { text.color -= color * Time.deltaTime; }
+    }
+}
--- Sand_Guardians/Assets/Scripts/TitleScene/SceneChangetoGS.cs
+++ Sand_Guardians/Assets/Scripts/TitleScene/SceneChangetoGS.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class SceneChangetoGS : MonoBehaviour
 {
+
+    // 音用変数
+    [SerializeField] private AudioClip buttonSE;
+
+    private AudioSource audioSource;
+
+
     void Start()
     {
-        
+        // コンポーネント取得
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C)) { FadeManager.Instance.LoadScene("GameScene", 0.5f); }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // シーン遷移
+            FadeManager.Instance.LoadScene("GameScene", 0.5f);
+
+            // ボタンSE
+            audioSource.PlayOneShot(buttonSE);
+        }
         
     }
 }
